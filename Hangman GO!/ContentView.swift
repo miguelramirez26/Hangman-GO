a335357391d4b23115aa534847b107b60d1b5339
miguelramirez26 <26ramirez.m@gmail.com>
2026-07-21 19:23:20 +0200

    Initial Commit

---
+++ Hangman GO!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hangman GO!
+//
+//  Created by Miguel Angel Ramirez Martinez on 21/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hangman GO!/Hangman_GO_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hangman_GO_App.swift
+//  Hangman GO!
+//
+//  Created by Miguel Angel Ramirez Martinez on 21/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Hangman_GO_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
